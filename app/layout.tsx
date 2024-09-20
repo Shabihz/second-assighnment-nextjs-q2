dbdf7fbc85036d31fe3afc52bdd9744b84830350
Shabih <syed.akbar@ptcl.net.pk>
2024-09-21 04:10:19 +0500

    second assignment of nextjs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-
+import Header from "./components/header";
+import About from "./components/about";
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -28,7 +29,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Header/>
         {children}
+        <About/>
       </body>
     </html>
   );
